04cf66f6346bc3537bd5fbbf512a0eeb5d44bbf8
Stephan Fuhrmann <stephan.fuhrmann@ionos.com>
2020-11-23 08:17:07 +0100

    New scripts

---
+++ scripts/pull.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+BRANCHES=$(git branch --format "%(upstream)" | sed -ne"s#^refs/remotes/origin/\([0-9.]*\)\$#\1#p")
+for i in ${BRANCHES}; do
+    echo "#########"
+    echo "Branch $i"
+    git checkout "$i"
+    git pull
+done
+git checkout master
