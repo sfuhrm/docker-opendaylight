b341e6e9757d8defa8ae8ad78b0763c3c3e7b4e1
Stephan Fuhrmann <stephan.fuhrmann@ionos.com>
2021-07-29 14:16:50 +0200

    Remove dockerhub specific stuff Add 0.8.x versions

--- scripts/call-build-hook.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-BASEDIR=$(cd $(dirname "$0"); cd ..; pwd)
-export DOCKERFILE_PATH=${BASEDIR}/Dockerfile
-export IMAGE_NAME=sfuhrm/opendaylight:latest
-export DOCKER_TAG=latest
-
-$BASEDIR/hooks/build
--- scripts/delete-create-branches.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-git push
-BASEDIR=$(cd $(dirname "$0"); cd ..; pwd)
-for t in $($BASEDIR/scripts/tags.sh); do
-	echo Branch: $t
-	git push origin --delete $t
-	git push origin master:$t
-done
